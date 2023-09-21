826a4b11807f514ac10dee8c26bfb8b8c92b1eba
Neural-Enigma <fcad.td@gmail.com>
2023-09-21 12:59:44 +0300

    Update second labwork codebase

--- Labwork-1-Java-Console-Book-Manager/src/Labwork_2/Main.java
+++ Labwork-1-Java-Console-Book-Manager/src/Labwork_2/Main.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество контейнеров для погрузки:");
-
         AtomicInteger totalContainers = new AtomicInteger(scanner.nextInt());
 
         System.out.println("Введите вместимость кораблей через пробел (4 числа):");
@@ -24,62 +24,22 @@ public class Main {
 
         BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 
-        Thread[] ships = new Thread[4];
-        int[] trips = new int[4];
+        Thread[] shipsThreads = new Thread[4];
+        Ship[] ships = new Ship[4];
 
         long startTime = System.currentTimeMillis();
 
         Object lock = new Object();
 
         for (int i = 0; i < 4; i++) {
-            final int index = i;
-            ships[i] = new Thread(() -> {
-                while (true) {
-                    if (totalContainers.get() <= 0) {
-                        String output = "Корабль с вместимостью " + capacities[index] + " завершил работу.";
-                        System.out.println(output);
-                        try {
-                            writer.write(output + "\n");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        break;
-                    }
-                    int containersToLoad;
-                    synchronized (lock) {
-                        if (totalContainers.get() <= 0) {
-                            break;
-                        }
-                        containersToLoad = Math.min(capacities[index], totalContainers.get());
-                        totalContainers.addAndGet(-containersToLoad);
-                        String output = "Осталось " + totalContainers.get() + " контейнеров.";
-                        System.out.println(output);
-                        try {
-                            writer.write(output + "\n");
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    try {
-                        Thread.sleep((long) loadingTime * containersToLoad);
-
-                        Thread.sleep(deliveryTime);
-
-                        trips[index]++;
-                        String output = "Корабль с вместимостью " + capacities[index] + " завершил рейс номер " + trips[index];
-                        System.out.println(output);
-                        writer.write(output + "\n");
-                    } catch (InterruptedException | IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
-            ships[i].start();
+            ships[i] = new Ship(totalContainers, capacities[i], writer, lock, loadingTime, deliveryTime);
+            shipsThreads[i] = new Thread(ships[i]);
+            shipsThreads[i].start();
         }
 
-        for (Thread ship : ships) {
+        for (Thread shipThread : shipsThreads) {
             try {
-                ship.join();
+                shipThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -90,8 +50,8 @@ public class Main {
         System.out.println(output);
         writer.write(output + "\n");
 
-        for (int i = 0; i < 4; i++) {
-            output = "Корабль с вместимостью " + capacities[i] + " сделал " + trips[i] + " рейсов.";
+        for (Ship ship : ships) {
+            output = "Корабль с вместимостью " + ship.getCapacity() + " сделал " + ship.getTrips() + " рейсов.";
             System.out.println(output);
             writer.write(output + "\n");
         }
---
+++ Labwork-1-Java-Console-Book-Manager/src/Labwork_2/Ship.java
@@ -0,0 +1,69 @@
+package Labwork_2;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class Ship implements Runnable {
+    private final AtomicInteger totalContainers;
+    private final int capacity;
+    private final BufferedWriter writer;
+    private final Object lock;
+    private final int loadingTime;
+    private final int deliveryTime;
+    private int trips;
+
+    public Ship(AtomicInteger totalContainers, int capacity, BufferedWriter writer, Object lock, int loadingTime, int deliveryTime) {
+        this.totalContainers = totalContainers;
+        this.capacity = capacity;
+        this.writer = writer;
+        this.lock = lock;
+        this.loadingTime = loadingTime;
+        this.deliveryTime = deliveryTime;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (totalContainers.get() <= 0) {
+                printAndWrite("Корабль с вместимостью " + capacity + " завершил работу.");
+                break;
+            }
+            int containersToLoad;
+            synchronized (lock) {
+                if (totalContainers.get() <= 0) {
+                    break;
+                }
+                containersToLoad = Math.min(capacity, totalContainers.get());
+                totalContainers.addAndGet(-containersToLoad);
+                printAndWrite("Осталось " + totalContainers.get() + " контейнеров.");
+            }
+            try {
+                Thread.sleep((long) loadingTime * containersToLoad);
+                Thread.sleep(deliveryTime);
+
+                trips++;
+                printAndWrite("Корабль с вместимостью " + capacity + " завершил рейс номер " + trips);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void printAndWrite(String message) {
+        System.out.println(message);
+        try {
+            writer.write(message + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getTrips() {
+        return trips;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
