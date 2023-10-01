d6106a4e39f4bfb79677cfc1fd9b4f949ca6371f
Neural-Enigma <fcad.td@gmail.com>
2023-10-02 02:03:07 +0300

    feat: implemented 'interesting' dependency

--- Labwork/src/main/java/bsuir/labwork/Labwork/models/CreditCard.java
+++ Labwork/src/main/java/bsuir/labwork/Labwork/models/CreditCard.java
@@ -4,6 +4,10 @@ import bsuir.labwork.Labwork.exceptions.InvalidCardException;
 import bsuir.labwork.Labwork.interfaces.Visitor;
 import jakarta.persistence.*;
 import lombok.Data;
+//Интересная зависимость
+//Удачно совпало с темой
+//Алгоритм Лу́на — алгоритм вычисления контрольной цифры номера пластиковой карты в соответствии со стандартом ISO/IEC 7812. Не является криптографическим средством, а предназначен в первую очередь для выявления ошибок, вызванных непреднамеренным искажением данных
+import org.apache.commons.validator.CreditCardValidator;
 
 import java.util.regex.Pattern;
 
@@ -20,21 +24,26 @@ public class CreditCard {
     private String expirationDate;
     private String cvc;
 
-    private static final String CARD_NUMBER_PATTERN = "\\d{4}-\\d{4}-\\d{4}-\\d{4}";
+    //private static final String CARD_NUMBER_PATTERN = "\\d{4}-\\d{4}-\\d{4}-\\d{4}";
     private static final String CARD_HOLDER_PATTERN = "[A-Za-z\\s]+";
     private static final String EXPIRATION_DATE_PATTERN = "\\d{4}-\\d{2}";
     private static final String CVC_PATTERN = "\\d{3}";
 
     public void validate() throws InvalidCardException {
-        if (!Pattern.matches(CARD_NUMBER_PATTERN, cardNumber)) {
+        CreditCardValidator cardValidator = new CreditCardValidator();
+
+        if (!cardValidator.isValid(cardNumber)) {
             throw new InvalidCardException("Invalid CardNumber");
         }
+
         if (!Pattern.matches(CARD_HOLDER_PATTERN, cardHolder)) {
             throw new InvalidCardException("Invalid CardHolder");
         }
+
         if (!Pattern.matches(EXPIRATION_DATE_PATTERN, expirationDate)) {
             throw new InvalidCardException("Invalid ExpirationDate");
         }
+
         if (!Pattern.matches(CVC_PATTERN, cvc)) {
             throw new InvalidCardException("Invalid CVC");
         }
