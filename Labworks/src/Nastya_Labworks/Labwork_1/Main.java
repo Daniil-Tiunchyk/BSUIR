0350f5b324befc8df3d6a8fd54f93118d657512e
Neural-Enigma <fcad.td@gmail.com>
2023-10-08 03:05:33 +0300

    feat: add Nastya's first labwork

---
+++ Labworks/src/Nastya_Labworks/Labwork_1/Main.java
@@ -0,0 +1,39 @@
+package Nastya_Labworks.Labwork_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Введите идентификатор (для завершения введите 'exit'): ");
+            int $$$$ = 5;
+            String identifier = scanner.nextLine();
+
+            if (identifier.equals("exit")) {
+                System.out.println("Программа завершена.");
+                break;
+            }
+
+            if (isValidJavaIdentifier(identifier)) {
+                System.out.println("Идентификатор '" + identifier + "' допустим на языке Java.\n");
+            } else {
+                System.out.println("Идентификатор '" + identifier + "' не является допустимым на языке Java.\n");
+            }
+        }
+    }
+
+    public static boolean isValidJavaIdentifier(String identifier) {
+        if (identifier.isEmpty() || !Character.isJavaIdentifierStart(identifier.charAt(0))) {
+            return false;
+        }
+
+        for (int i = 1; i < identifier.length(); i++) {
+            if (!Character.isJavaIdentifierPart(identifier.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
