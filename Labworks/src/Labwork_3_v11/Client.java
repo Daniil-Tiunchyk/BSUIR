ce439db9d9a093d7ee028ed7bd1ed0cdd589ea6a
Daniil-Tsiunchyk <fcad.td@gmail.com>
2023-12-07 18:18:01 +0300

    feat: added labwork_4 variant №11

--- Labworks/src/Labwork_3_v11/Client.java
+++ Labworks/src/Labwork_3_v11/Client.java
@@ -1,3 +1,5 @@
+package Labwork_3_v11;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
---
+++ Labworks/src/Labwork_4_v11/Client.java
@@ -0,0 +1,45 @@
+package Labwork_4_v11;
+
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        final String serverHostname = "localhost";
+        final int serverPort = 12345;
+        byte[] buffer = new byte[65507];
+
+        try {
+            InetAddress serverAddress = InetAddress.getByName(serverHostname);
+
+            try (DatagramSocket socket = new DatagramSocket()) {
+                socket.setSoTimeout(5000);
+
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Введите строку 1: ");
+                String line1 = scanner.nextLine().trim();
+                System.out.print("Введите строку 2: ");
+                String line2 = scanner.nextLine().trim();
+
+                String combinedLines = line1 + "\n" + line2;
+                byte[] dataToSend = combinedLines.getBytes(StandardCharsets.UTF_8);
+
+                DatagramPacket packetToSend = new DatagramPacket(dataToSend, dataToSend.length, serverAddress, serverPort);
+                socket.send(packetToSend);
+
+                DatagramPacket packetToReceive = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packetToReceive);
+
+                String responseData = new String(packetToReceive.getData(), 0, packetToReceive.getLength(), StandardCharsets.UTF_8);
+                System.out.println("Ответ от сервера:\n" + responseData);
+            }
+        } catch (SocketTimeoutException e) {
+            System.out.println("Время ожидания ответа истекло.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
